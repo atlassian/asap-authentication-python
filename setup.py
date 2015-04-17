ab166ed9b666c0db8067320f569f3b81cbcd5768
David Black <dblack@atlassian.com>
2015-04-17 16:50:46 +1000

    add a setup.py file.
    Signed-off-by: David Black <dblack@atlassian.com>

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+
+setup(
+    name='atlassian-jwt-auth',
+    packages=find_packages(),
+    version='0.0.1',
+    install_requires=[
+        'cryptography==0.8.2',
+        'PyJWT==1.1.0',
+        'requests==2.6.0',
+    ],
+    platforms=['any'],
+    license='MIT',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
