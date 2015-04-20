11273181dec50f774f04729f91d24b3517b35ca2
David Black <dblack@atlassian.com>
2015-04-20 15:27:53 +1000

    re-factor the TestJWTAuthVerifier class.
    Signed-off-by: David Black <dblack@atlassian.com>

--- atlassian_jwt_auth/tests/test_verifier.py
+++ atlassian_jwt_auth/tests/test_verifier.py
@@ -18,26 +18,30 @@ class TestJWTAuthVerifier(unittest.TestCase):
         self._private_key_pem = get_new_rsa_private_key_in_pem_format()
         self._public_key_pem = get_public_key_pem_for_private_key_pem(
             self._private_key_pem)
+        self._example_aud = 'aud_x'
+        self._example_issuer = 'egissuer'
+        self._example_key_id = '%s/a' % self._example_issuer
+        self._jwt_auth_signer = JWTAuthSigner(
+            self._example_issuer,
+            self._example_key_id,
+            self._private_key_pem.decode(),
+        )
 
     def _setup_mock_public_key_retriever(self, pub_key_pem):
         m_public_key_ret = mock.Mock()
         m_public_key_ret.retrieve.return_value = pub_key_pem.decode()
         return m_public_key_ret
 
+    def _setup_jwt_auth_verifier(self, pub_key_pem):
+        m_public_key_ret = self._setup_mock_public_key_retriever(pub_key_pem)
+        return JWTAuthVerifier(m_public_key_ret)
+
     def test_verify_claims_with_valid_jwt(self):
         """ test that verify_claims verifies a valid jwt. """
-        expected_audience = 'aud_x'
-        expected_issuer = 'issuer'
-        expected_key_id = '%s/a' % expected_issuer
-        m_public_key_ret = self._setup_mock_public_key_retriever(
-            self._public_key_pem)
-        verifier = JWTAuthVerifier(m_public_key_ret)
-        signer = JWTAuthSigner(
-            expected_issuer,
-            expected_key_id,
-            self._private_key_pem.decode())
-        signed_claims = signer.get_signed_claims(expected_audience)
-        v_claims = verifier.verify_claims(signed_claims, expected_audience)
+        verifier = self._setup_jwt_auth_verifier(self._public_key_pem)
+        signed_claims = self._jwt_auth_signer.get_signed_claims(
+            self._example_aud)
+        v_claims = verifier.verify_claims(signed_claims, self._example_aud)
         self.assertIsNotNone(v_claims)
-        self.assertEqual(v_claims['aud'], expected_audience)
-        self.assertEqual(v_claims['iss'], expected_issuer)
+        self.assertEqual(v_claims['aud'], self._example_aud)
+        self.assertEqual(v_claims['iss'], self._example_issuer)
