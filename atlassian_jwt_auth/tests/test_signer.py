e55dd97c79f75c2debf37eaca451e05fd9d809f5
David Black <dblack@atlassian.com>
2015-04-20 17:14:20 +1000

    update the test_signer code to use the new mixins.
    Signed-off-by: David Black <dblack@atlassian.com>

--- atlassian_jwt_auth/tests/test_signer.py
+++ atlassian_jwt_auth/tests/test_signer.py
@@ -5,18 +5,18 @@ import mock
 
 from ..signer import JWTAuthSigner
 from .utils import (
-    get_new_rsa_private_key_in_pem_format,
     get_example_jwt_auth_signer,
+    RS256KeyTestMixin,
+    ES256KeyTestMixin,
 )
 
 
-class TestJWTAuthSigner(unittest.TestCase):
+class BaseJWTAuthSignerTest(object):
 
     """ tests for the JWTAuthSigner class. """
 
     def setUp(self):
-        self.key = get_new_rsa_private_key_in_pem_format()
-        self.algorithm = 'RS256'
+        self._private_key_pem = self.get_new_private_key_in_pem_format()
 
     def test__get_claims(self):
         """ tests that _get_claims works as expected. """
@@ -27,7 +27,7 @@ class TestJWTAuthSigner(unittest.TestCase):
         jwt_auth_signer = JWTAuthSigner(
             expected_iss,
             expected_key_id,
-            key=self.key)
+            key=self._private_key_pem)
         jwt_auth_signer._now = lambda: expected_now
         expected_claims = {
             'iss': expected_iss,
@@ -46,7 +46,8 @@ class TestJWTAuthSigner(unittest.TestCase):
         """ tests that the jti of a claim changes. """
         expected_now = datetime.datetime(year=2001, day=1, month=1)
         aud = 'aud'
-        jwt_auth_signer = get_example_jwt_auth_signer()
+        jwt_auth_signer = get_example_jwt_auth_signer(
+            algorithm=self.algorithm, key=self._private_key_pem)
         jwt_auth_signer._now = lambda: expected_now
         first = jwt_auth_signer._get_claims(aud)['jti']
         second = jwt_auth_signer._get_claims(aud)['jti']
@@ -62,11 +63,29 @@ class TestJWTAuthSigner(unittest.TestCase):
         expected_key_id = 'key_id'
         expected_issuer = 'a_issuer'
         jwt_auth_signer = JWTAuthSigner(
-            expected_issuer, expected_key_id, key=self.key)
+            expected_issuer,
+            expected_key_id,
+            key=self._private_key_pem,
+            algorithm=self.algorithm,
+        )
         jwt_auth_signer._get_claims = lambda aud: expected_claims
         jwt_auth_signer.get_signed_claims(expected_aud)
         m_jwt_encode.assert_called_with(
             expected_claims,
-            key=self.key,
+            key=self._private_key_pem,
             algorithm=self.algorithm,
             headers={'kid': expected_key_id})
+
+
+class JWTAuthSignerRS256Test(
+        BaseJWTAuthSignerTest,
+        RS256KeyTestMixin,
+        unittest.TestCase):
+    pass
+
+
+class JWTAuthSignerES256Test(
+        BaseJWTAuthSignerTest,
+        ES256KeyTestMixin,
+        unittest.TestCase):
+    pass
