305072b406550d106aee2126a44c5948f6d766ea
David Black <dblack@atlassian.com>
2015-06-25 11:19:43 +1000

    Use pbr for setup configuration.
    Signed-off-by: David Black <dblack@atlassian.com>

--- setup.py
+++ setup.py
@@ -1,32 +1,12 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
-    name='atlassian-jwt-auth',
-    packages=find_packages(),
-    version=__import__('atlassian_jwt_auth').__version__,
-    install_requires=[
-        'cryptography==0.9',
-        'PyJWT==1.3.0',
-        'requests==2.7.0',
-        'CacheControl==0.11.5',
-    ],
+    setup_requires=['pbr==1.0.1'],
+    pbr=True,
     tests_require=['mock', 'nose', ],
     test_suite='nose.collector',
     platforms=['any'],
-    license='MIT',
     zip_safe=False,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'License :: OSI Approved :: MIT License',
-    ],
 )
