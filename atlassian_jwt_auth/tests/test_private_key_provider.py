be592390362aaa4483b9b2592ce72de63199cac5
David Black <dblack@atlassian.com>
2016-05-13 10:49:35 +1000

    Test that the DataUriPrivateKeyRetriever can be used  with a signer to generate a jwt.
    Signed-off-by: David Black <dblack@atlassian.com>

--- atlassian_jwt_auth/tests/test_private_key_provider.py
+++ atlassian_jwt_auth/tests/test_private_key_provider.py
@@ -5,6 +5,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.hazmat.primitives import serialization
 
+from atlassian_jwt_auth.signer import JWTAuthSigner
 from atlassian_jwt_auth.tests import utils
 from atlassian_jwt_auth.key import DataUriPrivateKeyRetriever
 
@@ -41,7 +42,18 @@ class BaseDataUriPrivateKeyRetrieverTest(object):
         provider = DataUriPrivateKeyRetriever(data_uri)
         kid, private_key_pem = provider.load('example')
         self.assertEqual(kid.key_id, expected_kid)
-        self.assertEqual(private_key_pem, self._private_key_pem)
+        self.assertEqual(private_key_pem,
+                         self._private_key_pem.decode('utf-8'))
+
+    def test_load_data_uri_can_be_used_with_a_signer(self):
+        """ tests that the data uri private key retriever can be used with a
+            signer to generate a jwt.
+        """
+        data_uri = self.get_example_data_uri(self._private_key_der)
+        provider = DataUriPrivateKeyRetriever(data_uri)
+        jwt_auth_signer = JWTAuthSigner(
+            'issuer', provider, algorithm=self.algorithm)
+        jwt_auth_signer.generate_jwt('aud')
 
 
 class DataUriPrivateKeyRetrieverRS256Test(BaseDataUriPrivateKeyRetrieverTest,
