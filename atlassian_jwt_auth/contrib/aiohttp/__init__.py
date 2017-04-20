932629a9dd39a89ef855815f4733b298a721ee7f
David Black <dblack@atlassian.com>
2017-04-21 09:21:12 +1000

    Warn when aiohttp is not installed so that the tests do not fail in python 3.5 when aiohttp is not installed.
    Signed-off-by: David Black <dblack@atlassian.com>

--- atlassian_jwt_auth/contrib/aiohttp/__init__.py
+++ atlassian_jwt_auth/contrib/aiohttp/__init__.py
@@ -2,8 +2,15 @@
 import sys
 
 if sys.version_info >= (3, 5):
-    from .auth import JWTAuth
-    from .key import HTTPSPublicKeyRetriever
-    from .verifier import JWTAuthVerifier
+    try:
+        import aiohttp
+        from .auth import JWTAuth
+        from .key import HTTPSPublicKeyRetriever
+        from .verifier import JWTAuthVerifier
+    except ImportError:
+        import warnings
+        warnings.warn(
+            'Could not import aiohttp code as aiohttp is not installed.')
+
 
 del sys
