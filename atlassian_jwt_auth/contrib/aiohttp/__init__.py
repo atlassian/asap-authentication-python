79b722dfcf781d8484c84215bdf49041da2684f5
David Black <dblack@atlassian.com>
2017-05-03 09:16:03 +1000

    Sem-Ver: bugfix Warn when an import error occurs when importing aiohttp so that the tests do not fail in python >= 3.5 when aiohttp is not installed.
    * Warn when an import error occurs when importing aiohttp so that the tests do not fail in python >= 3.5 when aiohttp is not installed.
    
    Signed-off-by: David Black <dblack@atlassian.com>

--- atlassian_jwt_auth/contrib/aiohttp/__init__.py
+++ atlassian_jwt_auth/contrib/aiohttp/__init__.py
@@ -2,8 +2,14 @@
 import sys
 
 if sys.version_info >= (3, 5):
-    from .auth import JWTAuth
-    from .key import HTTPSPublicKeyRetriever
-    from .verifier import JWTAuthVerifier
+    try:
+        import aiohttp
+        from .auth import JWTAuth
+        from .key import HTTPSPublicKeyRetriever
+        from .verifier import JWTAuthVerifier
+    except ImportError as e:
+        import warnings
+        warnings.warn(str(e))
+
 
 del sys
