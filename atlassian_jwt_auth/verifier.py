39296ce9231c9c51a7f8840afa3c6eb86ea0c213
Erik van Zijst <erik.van.zijst@gmail.com>
2017-07-07 13:25:57 +0200

    Make _seen_jti a ringbuffer.
    Switched to an OrderedDict to reduce jti lookups from n to log(n).

--- atlassian_jwt_auth/verifier.py
+++ atlassian_jwt_auth/verifier.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, OrderedDict
 
 import jwt
 
@@ -13,7 +13,7 @@ class JWTAuthVerifier(object):
     def __init__(self, public_key_retriever, **kwargs):
         self.public_key_retriever = public_key_retriever
         self.algorithms = algorithms.get_permitted_algorithm_names()
-        self._seen_jti = deque(maxlen=1000)
+        self._seen_jti = OrderedDict()
         self._subject_should_match_issuer = kwargs.get(
             'subject_should_match_issuer', True)
 
@@ -74,5 +74,7 @@ class JWTAuthVerifier(object):
         if _jti in self._seen_jti:
             raise ValueError("The jti, '%s', has already been used." % _jti)
         else:
-            self._seen_jti.append(_jti)
+            self._seen_jti[_jti] = None
+            while len(self._seen_jti) > 1000:
+                self._seen_jti.popitem(last=False)
         return claims
