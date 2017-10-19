90b2656cff5dcef422946eba9c2ba211c04958e6
Grant Mathews <grant.m.mathews@gmail.com>
2017-10-19 16:05:11 -0700

    Add ASAPMiddleware for Django
    Instead of relying on requires_asap() to validate claims after all the
    authentication middleware has run, introduce a middleware that can
    validate requests before they get to views at all.
    
    Sem-Ver: feature

--- atlassian_jwt_auth/contrib/django/middleware.py
+++ atlassian_jwt_auth/contrib/django/middleware.py
@@ -1,4 +1,14 @@
+import logging
+
+from jwt import InvalidAudienceError
+
 from django.conf import settings
+from django.http.response import HttpResponse
+from django.utils import six
+from jwt.exceptions import InvalidTokenError
+
+import atlassian_jwt_auth
+from .utils import parse_jwt, verify_issuers
 
 
 class ASAPForwardedMiddleware(object):
@@ -53,3 +63,75 @@ class ASAPForwardedMiddleware(object):
             request.META['HTTP_AUTHORIZATION'] = asap_auth
         if orig_auth is not None:
             request.META[self.xauth] = orig_auth
+
+
+class ASAPMiddleware(ASAPForwardedMiddleware):
+    """Enable ASAP for Django applications.
+
+    To use proxied credentials, this must come before any authentication
+    middleware.
+    """
+
+    def __init__(self, get_response=None):
+        super(ASAPMiddleware, self).__init__(get_response=get_response)
+
+        self.logger = logging.getLogger(__name__)
+
+        self.required = getattr(settings, 'ASAP_REQUIRED', True)
+        self.client_auth = getattr(settings, 'ASAP_CLIENT_AUTH', False)
+
+        # Configure verifier based on settings
+        retriever_kwargs = {}
+        retriever_cls = getattr(settings, 'ASAP_KEY_RETRIEVER_CLASS',
+                                atlassian_jwt_auth.HTTPSPublicKeyRetriever)
+        public_key_url = getattr(settings, 'ASAP_PUBLICKEY_REPOSITORY', None)
+        if public_key_url:
+            retriever_kwargs['base_url'] = public_key_url
+        retriever = retriever_cls(**retriever_kwargs)
+        self.verifier = atlassian_jwt_auth.JWTAuthVerifier(retriever)
+
+    def process_request(self, request):
+        auth_header = request.META.get('HTTP_AUTHORIZATION', b'')
+        # Per PEP-3333, headers must be in ISO-8859-1 or use an RFC-2047
+        # MIME encoding. We don't really care about MIME encoded
+        # headers, but some libraries allow sending bytes (Django tests)
+        # and some (requests) always send str so we need to convert if
+        # that is the case to properly support Python 3.
+        if isinstance(auth_header, six.string_types):
+            auth_header = auth_header.encode(encoding='iso-8859-1')
+        try:
+            scheme, auth = auth_header.split(b' ')
+        except ValueError:
+            scheme = b''
+
+        if scheme.lower() != b'bearer':
+            if not self.required:
+                return
+            message = 'Unauthorized: Invalid or missing token'
+            return HttpResponse(message, status=401)
+
+        try:
+            asap_claims = parse_jwt(self.verifier, auth)
+            verify_issuers(asap_claims)
+        except InvalidAudienceError as e:
+            if not self.required:
+                return
+            message = 'Forbidden: %s' % e
+            return HttpResponse(message, status=403)
+        except InvalidTokenError as e:
+            if not self.required:
+                return
+            message = 'Unauthorized: %s' % e
+            return HttpResponse(message, status=401)
+        except Exception as e:
+            # Something is rotten in the state of ASAP
+            self.logger.error(message,
+                              extra={'original_message': str(e)})
+            if not self.required:
+                return
+            raise
+
+        request.asap_claims = asap_claims
+
+        if self.client_auth:
+            super(ASAPMiddleware, self).process_request(request)
