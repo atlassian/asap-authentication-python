89b72b461d1279e3a606ce449f850dedc2a2d5c7
Grant Mathews <grant.m.mathews@gmail.com>
2017-10-18 17:24:48 -0700

    Add validate_asap() Django decorator
    requires_asap() assumes that nothing else has authenticated the request,
    which isn't always desirable.
    
    validate_asap() is a decorator that assumes something else has performed
    ASAP authentication and all valid claims are available in
    request.asap_claims.
    
    Sem-Ver: feature

--- atlassian_jwt_auth/contrib/django/decorators.py
+++ atlassian_jwt_auth/contrib/django/decorators.py
@@ -13,6 +13,47 @@ from atlassian_jwt_auth.exceptions import (PrivateKeyRetrieverException,
 from .utils import parse_jwt, verify_issuers
 
 
+def validate_asap(issuers=None, subjects=None, required=True):
+    """Decorator to allow endpoint-specific ASAP validation.
+
+    :param list issuers: A list of issuers that are allowed to use the
+        endpoint.
+    :param subject: A list of subjects or a function to determine allowed
+        subjects for the endpoint.
+    :param boolean required: Whether or not to require ASAP on this endpoint.
+        Note that requirements will be still be verified if claims are present.
+    """
+    def validate_asap_decorator(func):
+        @wraps(func)
+        def validate_asap_wrapper(request, *args, **kwargs):
+            asap_claims = getattr(request, 'asap_claims', {})
+            if required and not asap_claims:
+                message = 'Unauthorized: Invalid or missing token'
+                return HttpResponse(message, status=401)
+
+            iss = asap_claims.get('iss')
+            if issuers and iss not in issuers:
+                message = 'Unauthorized: Invalid token issuer'
+                return HttpResponse(message, status=401)
+
+            sub = asap_claims.get('sub')
+            if callable(subjects):
+                sub_allowed = subjects(sub)
+            elif isinstance(subjects, list):
+                sub_allowed = sub in subjects
+            else:
+                sub_allowed = True
+
+            if not sub_allowed:
+                message = 'Unauthorized: Invalid token subject'
+                return HttpResponse(message, status=401)
+
+            return func(request, *args, **kwargs)
+
+        return validate_asap_wrapper
+    return validate_asap_decorator
+
+
 def requires_asap(issuers=None):
     """Decorator for Django endpoints to require ASAP
 
