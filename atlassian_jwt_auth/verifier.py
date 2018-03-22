5b1d34006d24c61b8834decfc024a610825fa857
David Black <dblack@atlassian.com>
2018-03-22 14:16:49 +1100

    Remove the unneeded else clause from _check_jti as per pr feedback.
    Signed-off-by: David Black <dblack@atlassian.com>

--- atlassian_jwt_auth/verifier.py
+++ atlassian_jwt_auth/verifier.py
@@ -80,7 +80,6 @@ class JWTAuthVerifier(object):
         """Checks that the given jti has not been already been used."""
         if jti in self._seen_jti:
             raise ValueError("The jti, '%s', has already been used." % jti)
-        else:
-            self._seen_jti[jti] = None
-            while len(self._seen_jti) > 1000:
-                self._seen_jti.popitem(last=False)
+        self._seen_jti[jti] = None
+        while len(self._seen_jti) > 1000:
+            self._seen_jti.popitem(last=False)
