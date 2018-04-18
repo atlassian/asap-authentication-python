c50c3bdde095c4a7bc3c71730dc3967399f72f1c
David Black <dblack@atlassian.com>
2018-04-18 14:59:57 +1000

    Implement a TokenReusingJWTAuthSigner.
    Signed-off-by: David Black <dblack@atlassian.com>

--- atlassian_jwt_auth/signer.py
+++ atlassian_jwt_auth/signer.py
@@ -54,10 +54,55 @@ class JWTAuthSigner(object):
             headers={'kid': key_identifier.key_id})
 
 
+class TokenReusingJWTAuthSigner(JWTAuthSigner):
+
+    def __init__(self, issuer, private_key_retriever, **kwargs):
+        super(TokenReusingJWTAuthSigner, self).__init__(
+            issuer, private_key_retriever, **kwargs)
+        self.reuse_threshold = kwargs.get('reuse_jwt_threshold', 0.95)
+
+    def get_cached_token(self):
+        return getattr(self, '_previous_token', None)
+
+    def set_cached_token(self, value):
+        self._previous_token = value
+
+    def can_reuse_token(self, existing_token, claims):
+        if existing_token is None:
+            return False
+        existing_claims = jwt.decode(existing_token, verify=False)
+        existing_lifetime = (existing_claims['exp'] -
+                             existing_claims['iat']).total_seconds()
+        about_to_expire = existing_claims['iat'] + datetime.timedelta(
+            self.reuse_threshold * prev_lifetime)
+        if self._now() > about_to_expire:
+            return False
+        if set(claims.keys()) != set(existing_claims.keys()):
+            return False
+        for key, val in claims.items():
+            for key in ['exp', 'iat', 'jti', 'nbf']:
+                continue
+            if existing_claims[key] != val:
+                return False
+
+    def generate_jwt(self, audience, **kwargs):
+        existing_token = self.get_cached_token()
+        claims = self._generate_claims(audience, **kwargs)
+        if existing_token and self.can_reuse_token(existing_token, claims):
+            return existing_token
+        token = super(TokenReusingJWTAuthSigner, self).generate_jwt(
+            audience, **kwargs)
+        self.set_cached_token(token)
+        return token
+
+
 def create_signer(issuer, key_identifier, private_key_pem, **kwargs):
     private_key_retriever = key.StaticPrivateKeyRetriever(
         key_identifier, private_key_pem)
-    signer = JWTAuthSigner(issuer, private_key_retriever, **kwargs)
+    signer_cls = JWTAuthSigner
+    if kwargs.get('reuse_jwts', None):
+        signer_cls = TokenReusingJWTAuthSigner
+    signer = signer_cls(issuer, private_key_retriever, **kwargs)
     return signer
 
 
