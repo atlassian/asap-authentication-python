2644b07091c03b48dc43a349c703b0f8236c5469
David Black <dblack@atlassian.com>
2020-02-27 09:19:48 +1100

    Sem-Ver: feature Allow SettingsDict instances to be hashed.
    Signed-off-by: David Black <dblack@atlassian.com>

--- atlassian_jwt_auth/frameworks/common/utils.py
+++ atlassian_jwt_auth/frameworks/common/utils.py
@@ -7,3 +7,17 @@ class SettingsDict(dict):
 
     def __setitem__(self, key, value):
         raise AttributeError('SettingsDict properties are immutable')
+
+    def _hash_key(self):
+        keys_and_values = []
+        for key, value in self.items():
+            if isinstance(value, set):
+                value = frozenset(value)
+            keys_and_values.append("%s %s" % (key, hash(value)))
+        return frozenset(keys_and_values)
+
+    def __hash__(self):
+        return hash(self._hash_key())
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
