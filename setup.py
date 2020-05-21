d716de41f99face3e2f12d77547c46c83bc4bbb6
David Black <dblack@atlassian.com>
2020-05-21 15:27:04 +1000

    Sem-Ver: bugfix In CI move to using setup.py nosetests as setup.py test has been deprecated.
    Signed-off-by: David Black <dblack@atlassian.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(
-    setup_requires=['pbr<=6.0.0'],
+    setup_requires=['pbr<=6.0.0', 'nose'],
     pbr=True,
     test_suite='nose.collector',
     platforms=['any'],
