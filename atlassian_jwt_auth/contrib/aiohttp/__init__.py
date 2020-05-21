331cefb9ce3dd090bbeba5a18baff711692c498d
David Black <dblack@atlassian.com>
2020-05-21 13:11:01 +1000

    Sem-Ver: bugfix Fix some issues that flake8 detected.
    Signed-off-by: David Black <dblack@atlassian.com>

--- atlassian_jwt_auth/contrib/aiohttp/__init__.py
+++ atlassian_jwt_auth/contrib/aiohttp/__init__.py
@@ -3,10 +3,10 @@ import sys
 
 if sys.version_info >= (3, 5):
     try:
-        import aiohttp
-        from .auth import JWTAuth
-        from .key import HTTPSPublicKeyRetriever
-        from .verifier import JWTAuthVerifier
+        import aiohttp  # noqa
+        from .auth import JWTAuth  # noqa
+        from .key import HTTPSPublicKeyRetriever  # noqa
+        from .verifier import JWTAuthVerifier  # noqa
     except ImportError as e:
         import warnings
         warnings.warn(str(e))
--- atlassian_jwt_auth/contrib/tests/aiohttp/__init__.py
+++ atlassian_jwt_auth/contrib/tests/aiohttp/__init__.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 5):
     raise nose.SkipTest('Skipping tests for Python version < 3.5')
 else:
     try:
-        import asynctest
+        import asynctest  # noqa
     except ImportError:
         raise nose.SkipTest(
             'Skipping tests because asynctest is not installed')
