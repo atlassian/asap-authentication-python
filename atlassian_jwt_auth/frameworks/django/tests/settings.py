4b94bb3457c0815ef4f7148f1b75fb6e1ef1d29b
David Black <dblack@atlassian.com>
2022-03-31 11:56:00 +1100

    Sem-Ver: bugfix upgrade the version of Django used for testing to be >= 3.2.9 and < 3.3.0
    Signed-off-by: David Black <dblack@atlassian.com>

--- atlassian_jwt_auth/frameworks/django/tests/settings.py
+++ atlassian_jwt_auth/frameworks/django/tests/settings.py
@@ -1,14 +1,15 @@
-import os
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q%ma8co5d88eya9*cyj60mnwpfk8sbl-sqr2!^(musd0gr9)&1'
+SECRET_KEY = 'django-insecure-5i@^w(cnsaqrx*3co@!&wd' \
+             'vgp4wflkgw$qt#9j@e#tyxg!wdzd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -56,7 +57,7 @@ TEMPLATES = [
 ]
 
 # Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -67,7 +68,7 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -90,7 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -104,10 +105,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 
 ASAP_VALID_AUDIENCE = 'server-app'
 ASAP_VALID_ISSUERS = ('client-app', 'whitelist')
 ASAP_PUBLICKEY_REPOSITORY = None
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- atlassian_jwt_auth/frameworks/django/tests/urls.py
+++ atlassian_jwt_auth/frameworks/django/tests/urls.py
@@ -1,29 +1,29 @@
-from django.conf.urls import url
+from django.urls import path
 
 from atlassian_jwt_auth.frameworks.django.tests import views
 
 
 urlpatterns = [
-    url(r'^asap/expected$', views.expected_view, name='expected'),
-    url(r'^asap/unexpected$', views.unexpected_view, name='unexpected'),
-    url(r'^asap/decorated$', views.decorated_view, name='decorated'),
-    url(r'^asap/settings$', views.settings_view, name='settings'),
+    path('asap/expected', views.expected_view, name='expected'),
+    path(r'^asap/unexpected', views.unexpected_view, name='unexpected'),
+    path('^asap/decorated', views.decorated_view, name='decorated'),
+    path('asap/settings', views.settings_view, name='settings'),
 
-    url(r'^asap/subject_does_not_need_to_match_issuer$',
-        views.subject_does_not_need_to_match_issuer_view,
-        name='subject_does_not_need_to_match_issuer'),
-    url(r'^asap/subject_does_need_to_match_issuer_view$',
-        views.subject_does_need_to_match_issuer_view,
-        name='subject_does_need_to_match_issuer'),
+    path('asap/subject_does_not_need_to_match_issuer',
+         views.subject_does_not_need_to_match_issuer_view,
+         name='subject_does_not_need_to_match_issuer'),
+    path('asap/subject_does_need_to_match_issuer_view',
+         views.subject_does_need_to_match_issuer_view,
+         name='subject_does_need_to_match_issuer'),
 
-    url(r'^asap/subject_does_not_need_to_match_issuer_from_settings$',
-        views.subject_does_not_need_to_match_issuer_from_settings_view,
-        name='subject_does_not_need_to_match_issuer_from_settings'),
+    path('asap/subject_does_not_need_to_match_issuer_from_settings',
+         views.subject_does_not_need_to_match_issuer_from_settings_view,
+         name='subject_does_not_need_to_match_issuer_from_settings'),
 
-    url(r'^asap/needed$', views.needed_view, name='needed'),
-    url(r'^asap/unneeded$', views.unneeded_view, name='unneeded'),
-    url(r'^asap/restricted_issuer$', views.restricted_issuer_view,
-        name='restricted_issuer'),
-    url(r'^asap/restricted_subject$', views.restricted_subject_view,
-        name='restricted_subject'),
+    path('asap/needed', views.needed_view, name='needed'),
+    path(r'asap/unneeded', views.unneeded_view, name='unneeded'),
+    path(r'asap/restricted_issuer', views.restricted_issuer_view,
+         name='restricted_issuer'),
+    path('asap/restricted_subject', views.restricted_subject_view,
+         name='restricted_subject'),
 ]
