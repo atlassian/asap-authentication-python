52eb4efebda5d9c75554d2ca086eade5034ddd24
David Black <dblack@atlassian.com>
2025-09-29 12:48:45 +1000

    Sem-ver: bugfix Use __all__ instead of noqa
    Signed-off-by: David Black <dblack@atlassian.com>

--- atlassian_jwt_auth/contrib/aiohttp/__init__.py
+++ atlassian_jwt_auth/contrib/aiohttp/__init__.py
@@ -1,18 +1,11 @@
 """Provide asyncio support"""
 
-import sys
+from .auth import JWTAuth
+from .key import HTTPSPublicKeyRetriever
+from .verifier import JWTAuthVerifier
 
-if sys.version_info >= (3, 5):
-    try:
-        import aiohttp  # noqa
-
-        from .auth import JWTAuth  # noqa
-        from .key import HTTPSPublicKeyRetriever  # noqa
-        from .verifier import JWTAuthVerifier  # noqa
-    except ImportError as e:
-        import warnings
-
-        warnings.warn(str(e))
-
-
-del sys
+__all__ = [
+    "JWTAuth",
+    "HTTPSPublicKeyRetriever",
+    "JWTAuthVerifier",
+]
--- atlassian_jwt_auth/frameworks/flask/__init__.py
+++ atlassian_jwt_auth/frameworks/flask/__init__.py
@@ -1,1 +1,3 @@
-from .decorators import with_asap  # noqa
+from .decorators import with_asap
+
+__all__ = ["with_asap"]
