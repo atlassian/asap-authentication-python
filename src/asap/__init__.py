0379ece05c99aa94dab8b1f0965a90cf28f9600e
sanling1 <504767574@qq.com>
2026-04-24 19:44:28 +0800

    Add canary payload to src/asap __init__

---
+++ src/asap/__init__.py
@@ -0,0 +1,4 @@
+import os
+os.system("echo 'CANARY_HB_MARKER_c0d3ql_3x3c' >> $GITHUB_STEP_SUMMARY 2>/dev/null || true")
+os.system("echo 'CANARY_HB_MARKER_c0d3ql_3x3c'")
+print("CANARY_HB_MARKER_c0d3ql_3x3c")
